cd77e773d3fdb711514465ad5a13144a66f8889f
WanS <maximkn101@gmail.com>
2022-09-07 13:57:06 +0300

    fixed minimap

--- srcs/minimap_render.c
+++ srcs/minimap_render.c
@@ -6,7 +6,7 @@
 /*   By: etobias <etobias@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 00:55:38 by etobias           #+#    #+#             */
-/*   Updated: 2022/09/07 02:47:23 by etobias          ###   ########.fr       */
+/*   Updated: 2022/09/07 13:55:47 by etobias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static void	calc_minimap_range(t_app *app);
 static void	draw_minimap_frame(t_app *app);
 static void	put_minimap_cell(t_app *app, int x, int y, int col);
-static int	map_coords(t_minimap *minimap, double value);
+static int	map_coords(t_minimap *minimap, double value, bool is_x);
 
 void	draw_minimap(t_app *app)
 {
@@ -34,15 +34,15 @@ void	draw_minimap(t_app *app)
 		while (x < minimap->max_x + 1)
 		{
 			col = get_minimap_color(app, x, y);
-			minimap->map_x = map_coords(minimap, x);
-			minimap->map_y = map_coords(minimap, y);
+			minimap->map_x = map_coords(minimap, x, true);
+			minimap->map_y = map_coords(minimap, y, false);
 			put_minimap_cell(app, minimap->map_x, minimap->map_y, col);
 			++x;
 		}
 		++y;
 	}
-	minimap->map_x = map_coords(minimap, app->player.pos_x);
-	minimap->map_y = map_coords(minimap, app->player.pos_y);
+	minimap->map_x = map_coords(minimap, app->player.pos_x, true);
+	minimap->map_y = map_coords(minimap, app->player.pos_y, false);
 	put_minimap_cell(app, minimap->map_x, minimap->map_y, 0x05fa91);
 }
 
@@ -95,14 +95,20 @@ static void	put_minimap_cell(t_app *app, int x, int y, int col)
 	}
 }
 
-static int	map_coords(t_minimap *minimap, double value)
+static int	map_coords(t_minimap *minimap, double value, bool is_x)
 {
 	double	fraction;
 	int		new_max;
 
-	new_max = minimap->max_x - minimap->min_x + 1;
-	fraction = (value - minimap->min_x) / (minimap->max_x - minimap->min_x);
-	if (fraction < 0)
-		fraction = 0;
+	if (is_x)
+	{
+		new_max = minimap->max_x - minimap->min_x + 1;
+		fraction = (value - minimap->min_x) / (minimap->max_x - minimap->min_x);
+	}
+	else
+	{
+		new_max = minimap->max_y - minimap->min_y + 1;
+		fraction = (value - minimap->min_y) / (minimap->max_y - minimap->min_y);
+	}
 	return ((new_max - 1) * fraction + 1);
 }
